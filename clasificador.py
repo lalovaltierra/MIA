2b6c635b91f96bbcd923ca84a8f76d87c8c2bd56
lalovaltierra <58576490+lalovaltierra@users.noreply.github.com>
2024-01-09 14:11:22 -0600

    Delete clasificador.py

--- clasificador.py
+++
@@ -1,5 +0,0 @@
-# clasificador.py
-def contar_pixeles(imagen):
-    # Lógica para contar píxeles (aquí puedes implementar tu propio algoritmo)
-    # Este ejemplo simplemente devuelve la longitud del archivo de la imagen
-    return len(imagen.read())
